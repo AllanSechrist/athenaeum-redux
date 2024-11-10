edbed4644a88d493c2b9060320019d40bffb6e3a
AllanSechrist <sechristallan@gmail.com>
2024-11-10 17:04:02 +0900

    add book detail view

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
 
 from .models import Book
 
@@ -8,3 +9,9 @@ class BookListView(ListView):
     model = Book
     template_name = "book_list.html"
     context_object_name = "book_list"
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "book_detail.html"
+    context_object_name = "book"
+    
