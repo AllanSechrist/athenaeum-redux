bb1f9b26bbce0709991f24894a354352a86dc8de
AllanSechrist <sechristallan@gmail.com>
2024-11-10 17:59:19 +0900

    add add_book view

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, BookCreateView
 
 urlpatterns = [
     path("", BookListView.as_view() ,name="book_list"),
     path("<int:pk>", BookDetailView.as_view(), name="book_detail"),
+    path("add/", BookCreateView.as_view(), name="add_book"),
     # path("<int:book_id>", views.detail, name="detail"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 
 from .models import Book
@@ -14,4 +14,13 @@ class BookDetailView(DetailView):
     model = Book
     template_name = "book_detail.html"
     context_object_name = "book"
-    
+
+class BookCreateView(CreateView):
+    model = Book
+    fields = ["title", "author", "publisher", "isbn"]
+    template_name = "add_book.html"
+    success_url = reverse_lazy("book_list")
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
